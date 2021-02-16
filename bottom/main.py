4cadd541c7a1bd3c86ee5e3562476403dbf40651
superucitelka <dokonala.hilda@gmail.com>
2021-02-16 18:31:36 +0100

    02 - theming, intro

--- bottom/main.py
+++ bottom/main.py
@@ -1,11 +1,28 @@
+from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
 from kivy.lang import Builder
+
+
+class TextScreen(Screen):
+    pass
+
+
+class FormScreen(Screen):
+    pass
+
+
+class TableScreen(Screen):
+    pass
+
+
+class MainScreen(Screen):
+    pass
 
 
 class Test(MDApp):
 
     def build(self):
-        self.theme_cls.primary_palette = "Yellow"
+        self.theme_cls.primary_palette = "Gray"
         return Builder.load_file('bottom.kv')
 
 
